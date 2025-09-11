b6fb57d7c1f37e69d853b5a5cb23fb9750645269
Glego <glego200323@gmail.com>
2025-09-11 16:05:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
+import requests
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    api_key = "f89358b2221f41f8a6c130205251109"
+    if not api_key:
+        raise ValueError("API_KEY is not set in environment variables")
+
+    url = f"http://api.weatherapi.com/v1/current.json"
+    params = {"key": api_key, "q": "Paris", "aqi": "no"}
+
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+
+    city = data["location"]["name"]
+    temp_c = data["current"]["temp_c"]
+    condition = data["current"]["condition"]["text"]
+
+    print(f"Weather in {city}: {temp_c}°C, {condition}")
 
 
 if __name__ == "__main__":
